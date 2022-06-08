5b3e12cd2258e3a8d183205df7de0c2d168b8d49
James Pehrson <jamesbpehrson88@gmail.com>
2022-06-08 16:01:45 -0600

    finished intermediate section

--- index.js
+++ index.js
@@ -33,15 +33,27 @@ function deleteMovie(event) {
     event.target.parentNode.remove();
 
     message.textContent = 'Movie Deleted'
+    revealMessage()
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`
 }
 
 function crossOffMovie(event) {
 event.target.classList.toggle('checked')
 if (event.target.classList.contains('checked') === true) {
     message.textContent = 'Movie Watched'
-
+    message.textContent = `${event.target.textContent} watched`
 
 }else {
     message.textContent = 'Movie Added Back'
+    message.textContent = `${event.target.textContent} added back!`
 }
+revealMessage()
+}
+
+function revealMessage(){
+
+    message.classList.remove('hide')
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000)
 }
